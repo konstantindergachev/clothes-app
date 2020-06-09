fbe20c8c88f50f563a5e8eef4fcb238d75874055
Konstantin Dergachev <dergachevkonstantin@gmail.com>
2020-06-09 09:35:04 +0300

    Change date calculation and upgrade packages

--- client/src/layout/footer/Footer.js
+++ client/src/layout/footer/Footer.js
@@ -4,9 +4,7 @@ import './Footer.scss';
 const Footer = () => {
   return (
     <footer className="footer">
-      <h4 className="footer-left">
-        &copy; {new Date().toISOString().split('-')[0]} Харьков
-      </h4>
+      <h4 className="footer-left">&copy; {new Date().getFullYear()} Харьков</h4>
       <h4 className="footer-right">Гардероб</h4>
     </footer>
   );
